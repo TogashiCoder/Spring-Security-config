1f06ec6ae7f14f0560d001a086292c2a40406da2
Togahsi <silverproduction2023@gmail.com>
2024-07-11 16:09:40 +0100

    full config

---
+++ security/src/main/java/security/test/TestController.java
@@ -0,0 +1,17 @@
+package security.test;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> alloHello(){
+        return ResponseEntity.ok("allo mn dakhl li nadi w securiser");
+    }
+}
